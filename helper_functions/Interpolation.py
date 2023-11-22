added oversampling, fixed a bug(Dicom rotation)

helper_functions/Interpolation.py
@@ -157,12 +157,17 @@ def points_rps2xyz(scan_index = 0, twix = None,
     return points_xyz.reshape((-1,)+RR.shape) , rotmatrix, offset , img_quat, geo.normal
 
 import matplotlib.pyplot as plt
-def interpolation(twix, num_sli, n, input_data, oversampling_phase_factor = 3):
+def interpolation(twix, dim_info_org, num_sli, n, input_data, oversampling_phase_factor = 3):
     points_3d_xyz , rotmatrix3d, offset, _,_ = points_rps2xyz(0,twix= twix,num_sli = num_sli, n = n)
 
     points_2d_xyz , *_ , img_quat, normal = points_rps2xyz(1, twix= twix,rotmatrix_3d = rotmatrix3d, offset_0 = offset,num_sli = num_sli, n = n, oversampling_phase_factor = oversampling_phase_factor)
     points_2d_xyz = points_2d_xyz.transpose([1,2,3,0])#.reshape((-1,3))
-    points_2d_xyz = np.mean(points_2d_xyz,2)
+    #when dim_info_org.index('Par') has a value and the value is bigger than 1 we don't need points_2d_xyz = np.mean(points_2d_xyz,2), the 'Par' doesn't always exist in the dim_info_org
+    try:
+        if points_2d_xyz.shape[dim_info_org.index('Par')] < 1:
+            points_2d_xyz = np.mean(points_2d_xyz,2)
+    except:
+        points_2d_xyz = np.mean(points_2d_xyz,2)
 
     output_data = []
     for data in input_data:
@@ -187,7 +192,27 @@ def cut_3D_cube(tmp_image_3D, points_3d_xyz, points_2d_xyz):
         interpolator = RegularGridInterpolator((points_3d_xyz[0, 0, :, 0],
                                         points_3d_xyz[1, :, 0, 0],
                                         points_3d_xyz[2, 0, 0, :]),
-                                        abs(tmp_image_3D[:,:,::-1,coil_idx]),  bounds_error=False,fill_value = np.nan) #[:,:,::-1]
+                                        abs(tmp_image_3D[:,:,::-1,coil_idx]),  bounds_error=False,fill_value = 0)#np.nan) #[:,:,::-1]
         zi_image = interpolator(points_2d_xyz[...,[0,1,2]])
         interpolated_2D_img[:,:,coil_idx] = zi_image.reshape(points_2d_xyz.shape[:2])
     return np.squeeze(interpolated_2D_img)
+
+
+# def cut_3D_cube(tmp_image_3D, points_3d_xyz, points_2d_xyz):
+#     try:
+#         dummy_var = tmp_image_3D.shape[3]
+#     except:
+#         tmp_image_3D = np.expand_dims(tmp_image_3D, axis=3)
+
+#     interpolated_2D_img = np.zeros((points_2d_xyz.shape[0],points_2d_xyz.shape[1],points_2d_xyz.shape[2],tmp_image_3D.shape[3]))
+#     #print("points_2d_xyz.shape",points_2d_xyz.shape) #(96, 192, 72, 3)
+
+#     for coil_idx in range(tmp_image_3D.shape[3]):
+#         interpolator = RegularGridInterpolator((points_3d_xyz[0, 0, :, 0],
+#                                         points_3d_xyz[1, :, 0, 0],
+#                                         points_3d_xyz[2, 0, 0, :]),
+#                                         abs(tmp_image_3D[:,:,::-1,coil_idx]),  bounds_error=False,fill_value = np.nan) #[:,:,::-1]
+#         zi_image = interpolator(points_2d_xyz[...,[0,1,2]])
+#         np.save("correction_map.npy",zi_image)
+#         interpolated_2D_img[:,:,:,coil_idx] = zi_image.reshape(points_2d_xyz.shape[:-1])
+#     return np.squeeze(interpolated_2D_img)

helper_functions/calculating_correction_map.py
@@ -322,8 +322,8 @@ def calculate_correction_map_3D(x3_s_in, x3_b_in, lamb = 1e-3, tol=1e-4, maxiter
     #np.save('x3_s_in.npy', x3_s_in)
     #np.save('x3_b_in.npy', x3_b_in)
 
-    x3_s_in = extendTo128x64x64(x3_s_in)
-    x3_b_in = extendTo128x64x64(x3_b_in)
+    x3_s_in = np.abs(extendTo128x64x64(x3_s_in))
+    x3_b_in = np.abs(extendTo128x64x64(x3_b_in))
     #x3_s_in = extendTo128x128x128(x3_s_in)
     #x3_b_in = extendTo128x128x128(x3_b_in)
 
@@ -343,13 +343,26 @@ def calculate_correction_map_3D(x3_s_in, x3_b_in, lamb = 1e-3, tol=1e-4, maxiter
     else:
         x3_s = x3_s_in/np.max(x3_s_in)
         x3_b = x3_b_in/np.max(x3_s_in)
-        pass
+        
+    plt.figure(figsize=(5, 10))
+    plt.subplot(2, 2, 1)
+    plt.imshow(np.abs(np.squeeze(x3_s[:, 32, :]))**1, cmap='gray')
+    plt.axis('off')
+    plt.colorbar()
+    plt.title('surface coil')
+
+    plt.subplot(2, 2, 2)
+    plt.imshow(np.abs(np.squeeze(x3_b[:, 32, :]))**1, cmap='gray')
+    plt.axis('off')
+    plt.colorbar()
+    plt.title('body coil')
 
     
 
     if debug:
         print("*****************start********************")
         print(np.max(x3_s_in), np.max(x3_b_in))
+        print(np.max(x3_s), np.max(x3_b))
         print("******************end*******************")
         print("Shape of x3_s:", x3_s.shape)
         print("Shape of x3_b:", x3_b.shape)
@@ -426,7 +439,7 @@ def calculate_correction_map_3D(x3_s_in, x3_b_in, lamb = 1e-3, tol=1e-4, maxiter
         plt.imshow(np.squeeze(c[:, ny//2, :]), cmap='jet')
         plt.axis('off')
         plt.title('correction map')
-        #plt.colorbar()
+        plt.colorbar(shrink=0.7)
 
         plt.subplot(2, 2, 4)
         plt.imshow(np.squeeze(x3_sc[:, ny//2, :]), cmap='gray')

helper_functions/recon.py
@@ -16,12 +16,18 @@ from helper_functions.Interpolation import quaternion_to_directions
 #     return grappa_img
 
 def sense_reconstruction(ksp,ref_padded,inversed_correction_map = None,thresh=0.003,crop=0.0):
+    #print the type of ref_padded
+    print("type(ref_padded)",type(ref_padded))
     mps = mr.app.EspiritCalib(ref_padded,thresh=thresh,crop=crop).run()#(ref_padded,thresh=0.1,crop=0.50).run() #thresh=0.02,crop=0.05 good
+    print("mps.shape",mps.shape)
     if inversed_correction_map is not None:
-        mps = np.multiply(mps,inversed_correction_map[::-1,:])
+        try:
+            mps = np.multiply(mps,inversed_correction_map[:,:])
+        except:
+            mps = np.multiply(mps,inversed_correction_map[:,:].transpose([1,0]))
     sense_img = mr.app.SenseRecon(ksp, mps).run()
     sense_img = complex_image_normalization(sense_img)
-    sense_img = sense_img[::-1,:]
+    sense_img = sense_img[:,:]
 
     return sense_img
 
@@ -87,7 +93,7 @@ def remove_oversampling_phase_direction(data, oversampling_phase_factor = 3):
     # except:
     #     pass
 
-    # Calculate the sum of the images, ignoring nan values
+    # # Calculate the sum of the images, ignoring nan values
     summed_image = np.nansum(data, axis=0)
 
     # Calculate the number of non-nan values for each pixel