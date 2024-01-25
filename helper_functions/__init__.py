4056f4c86b3ac8e81627635910bc39dc8425438c
Xuan Lei <111537918+LajerQAQ@users.noreply.github.com>
2024-01-25 12:04:35 -0500

    removed temporary information

--- helper_functions/__init__.py
+++ helper_functions/__init__.py
@@ -1,4 +1,4 @@
-print("please read README.md to run the code.")
-print("The code is still under development.")
-print("plesae contact me for adding more data shape support.")
-print("lei.337@osu.edu")
+# print("please read README.md to run the code.")
+# print("The code is still under development.")
+# print("plesae contact me for adding more data shape support.")
+# print("lei.337@osu.edu")
